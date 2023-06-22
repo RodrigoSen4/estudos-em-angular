68fc818e18d51c12812bf7ee32826d843e3552c8
RodrigoSen4 <dias.rodrigosena@gmail.com>
2023-06-22 15:08:48 -0300

    Commit inicial, iniciando estudos em angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FristComponentComponent } from './components/frist-component/frist-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FristComponentComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/frist-component/frist-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frist-component',
+  templateUrl: './frist-component.component.html',
+  styleUrls: ['./frist-component.component.css']
+})
+export class FristComponentComponent {
+    name: string = 'Rodrigo';
+}
